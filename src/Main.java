f4c310c74d9f2134f3975eb868ff4f4ee3e76334
Fahim <fahim.cross@gmail.com>
2021-01-03 12:32:25 +0600

    Test 2

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
         for(int i = 0; i < 10; i++){
             System.out.println(i);
         }
+
+        System.out.println("Hello");
+        for(int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
